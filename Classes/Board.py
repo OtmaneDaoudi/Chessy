8149137165497481e5c8d8786867fc71f9da82ce
OtmanDaoudi <daoudiotman22@gmail.com>
2022-04-25 21:16:26 +0000

    ''

---
+++ Classes/Board.py
@@ -0,0 +1,10 @@
+import Piece
+import Square
+#an 8*8 representation of the board
+#the board is represented using a multi-dim list of cases
+class Board:
+    def __init__(self):
+        #initialise suqres
+        for line in range
+    
+        
---
+++ Classes/Square.py
@@ -0,0 +1,6 @@
+class Square:
+    def init(self,line,column,piece = None):
+        self.line = line
+        self.column = column
+        self.piece = piece
+        piece
