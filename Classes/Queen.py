2be341bd25368f9adc2c33582b144501a48b67ba
OtmanDaoudi <daoudiotman22@gmail.com>
2022-04-30 14:40:30 +0000

    added Queen movements

--- Classes/Queen.py
+++ Classes/Queen.py
@@ -1,4 +1,6 @@
+from Classes.Bishop import Bishop
 from Classes.Piece import Piece
+from Classes.Rook import Rook
 
 class Queen(Piece):
     def __init__(self,rank,column,color,image = None) : 
@@ -8,3 +10,12 @@ class Queen(Piece):
         if  (self.color == "w"): return f"{Piece.WHITE_QUEEN}"
         elif(self.color == "b"): return f"{Piece.BLACK_QUEEN}"
         else :return ""
+
+    def getPossibleMoves(self,board):
+        res = []
+        #the queen combines the moves of both a rook and a bishop
+        as_rook = Rook(self.rank, self.column, self.color)
+        as_bishop = Bishop(self.rank , self.column, self.color)
+        res.extend(as_rook.getPossibleMoves(board))
+        res.extend(as_bishop.getPossibleMoves(board))
+        return res
--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from Classes.Board import Board
 
 bd = Board()
 bd.printBoard()
-print(bd.board[0][1].getPossibleMoves(bd.board))
+bd.move_piece((1,3),(3,3))
+bd.move_piece((0,3),(1,3))
+bd.move_piece((1,3),(2,3))
+print(bd.board[2][3].getPossibleMoves(bd.board))
+bd.printBoard()
