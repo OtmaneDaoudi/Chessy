60303c196c7dc544acc78383eed5d32565604223
OtmanDaoudi <daoudiotman22@gmail.com>
2022-04-30 14:13:26 +0000

    added knight movements

--- Classes/Knight.py
+++ Classes/Knight.py
@@ -9,3 +9,35 @@ class Knight(Piece):
         if  (self.color == "w"): return f"{Piece.WHITE_KNIGHT}"
         elif(self.color == "b"): return f"{Piece.BLACK_KNIGHT}"
         else :return ""
+
+    def getPossibleMoves(self,board):
+        res = []
+        #up
+        if self.rank + 2 <= 7:
+            if self.column + 1 <= 7 and (board[self.rank + 2][self.column + 1] is None or board[self.rank + 2][self.column + 1].color != self.color):
+                res.append((self.rank + 2, self.column + 1))
+            if self.column - 1 >= 0 and (board[self.rank + 2][self.column - 1] is None or board[self.rank + 2][self.column - 1].color != self.color):
+                res.append((self.rank + 2, self.column - 1))
+
+        #right
+        if self.column + 2 <= 7:
+            if self.rank + 1 <= 7 and (board[self.rank + 1][self.column + 2] is None or board[self.rank + 1][self.column + 2].color != self.color):
+                res.append((self.rank + 1,self.column + 2))
+            if self.rank - 1 >= 0 and (board[self.rank - 1][self.column + 2] is None or board[self.rank - 1][self.column + 2].color != self.color):
+                res.append((self.rank - 1, self.column + 2))
+
+        #left
+        if self.column - 2 >= 0:
+            if self.rank + 1 <= 7 and (board[self.rank + 1][self.column -2] is None or board[self.rank + 1][self.column -2].color != self.color):
+                res.append((self.rank + 1, self.column - 2))
+            if self.rank - 1 >= 0 and (board[self.rank - 1][self.column - 2] is None or board[self.rank - 1][self.column - 2].color != self.color):
+                res.append((self.rank - 1, self.column - 2))
+
+        #down
+        if self.rank - 2 >= 0:
+            if self.column + 1 <= 7 and (board[self.rank - 2][self.column + 1] is None or board[self.rank - 2][self.column + 1].color != self.color):
+                res.append((self.rank - 2, self.column + 1))
+            if self.column - 1 >= 0 and (board[self.rank - 2][self.column - 1] is None or board[self.rank - 2][self.column - 1].color != self.color):
+                res.append((self.rank - 2, self.column - 1))
+
+        return res
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from Classes.Board import Board
 
 bd = Board()
 bd.printBoard()
+print(bd.board[0][1].getPossibleMoves(bd.board))
