6e438100210e6ec82a1eb156e69f4d7cf7ab99f2
OtmanDaoudi <daoudiotman22@gmail.com>
2022-06-22 22:00:59 +0100

    fixed stats

--- UI/StatsScreen.py
+++ UI/StatsScreen.py
@@ -42,7 +42,8 @@ class StatsScreen(Screen):
             data = Connection.getStats()
             # StatsScreen.playerName = str(stored_data.get('user1')['userName'])
             playerName = App.get_running_app().root.get_screen('stats').ids.playerName
-            playerName.text = str(stored_data.get('user1')['userName'])
+            playerName.text = "Statistiques du jeu, joueur : "+str(stored_data.get('user1')['userName'])
+
             PVPtotalPLayed =  App.get_running_app().root.get_screen('stats').ids.PVPtotalPLayed
             PVPwins =  App.get_running_app().root.get_screen('stats').ids.PVPwins
             PVPwinsPercentagePB =  App.get_running_app().root.get_screen('stats').ids.PVPwinsPercentagePB
@@ -58,42 +59,42 @@ class StatsScreen(Screen):
 
             PVMtotalPLayed =  App.get_running_app().root.get_screen('stats').ids.PVMtotalPLayed
             PVMwins =  App.get_running_app().root.get_screen('stats').ids.PVMwins
-            PVMwinsPB =  App.get_running_app().root.get_screen('stats').ids.PVMwinsPB
+            PVMwinsPercentagePB =  App.get_running_app().root.get_screen('stats').ids.PVMwinsPercentagePB
             PVMwinsPercentage =  App.get_running_app().root.get_screen('stats').ids.PVMwinsPercentage
+            PVMlost =  App.get_running_app().root.get_screen('stats').ids.PVMlost
             PVMlostPB =  App.get_running_app().root.get_screen('stats').ids.PVMlostPB
             PVMlostPercentage =  App.get_running_app().root.get_screen('stats').ids.PVMlostPercentage
             PVMdraw =  App.get_running_app().root.get_screen('stats').ids.PVMdraw
-            PVMdrawPG =  App.get_running_app().root.get_screen('stats').ids.PVMdrawPG
+            PVMdrawPB =  App.get_running_app().root.get_screen('stats').ids.PVMdrawPB
             PVMdrawPercentage =  App.get_running_app().root.get_screen('stats').ids.PVMdrawPercentage
             PVMbestScore =  App.get_running_app().root.get_screen('stats').ids.PVMbestScore
+            PVMbestTime =  App.get_running_app().root.get_screen('stats').ids.PVMbestTime
 
             # print("fetched data = ", data)
             pvp = data[0]
             pvm = data[1]
             #pvp data
-            StatsScreen.PVPtotalPLayed = pvp[1]
-            StatsScreen.PVPwins = pvp[2]
-            StatsScreen.PVPlost = pvp[5]
-            StatsScreen.PVPdraw = pvp[6]
-            if StatsScreen.PVPtotalPLayed != 0:
-                StatsScreen.PVPwinsPercentage = StatsScreen.PVPwins/StatsScreen.PVPtotalPLayed*100
-                StatsScreen.PVPlostPercentage = StatsScreen.PVPlost/StatsScreen.PVPtotalPLayed*100
-                StatsScreen.PVPdrawPercentage = StatsScreen.PVPdraw/StatsScreen.PVPtotalPLayed*100
-            StatsScreen.PVPbestScore = pvp[3]
+            PVPtotalPLayed.text = str(pvp[1])
+            PVPwins.text = str(pvp[2])
+            PVPlost.text = str(pvp[5])
+            PVPdraw.text = str(pvp[6])
+            if pvp[1] != 0:
+                PVPwinsPercentagePB.value,PVPwinsPercentage.text = int(pvp[2]/pvp[1]*100), str(int(pvp[2]/pvp[1]*100))+"%"
+                PVPlostPB.value, PVPlostPercentage.text = int(pvp[5]/pvp[1]*100), str(int(pvp[5]/pvp[1]*100))+"%"
+                PVPdrawPB.value, PVPdrawPercentage.text = int(pvp[6]/pvp[1]*100), str(int(pvp[6]/pvp[1]*100))+"%"
+            PVPbestScore.text = str(pvp[3])+"pts | "+str(divmod(pvp[7], 60)[0])+"m"+str(divmod(pvp[7], 60)[1])+"s" 
             
-            StatsScreen.PVPbestScoreTime = pvp[7]
-            StatsScreen.PVPbestTime = pvp[8]
-    
-            #pvm data 
-            pvm = data[1]
-            StatsScreen.PVMtotalPLayed = pvm[1]
-            StatsScreen.PVMwins = pvm[2]
-            StatsScreen.PVMlost = pvm[5]
-            StatsScreen.PVMdraw = pvm[6]
-            if StatsScreen.PVMtotalPLayed != 0:
-                StatsScreen.PVMwinsPercentage = StatsScreen.PVMwins/StatsScreen.PVMtotalPLayed*100
-                StatsScreen.PVMdrawPercentage = StatsScreen.PVMdraw/StatsScreen.PVMtotalPLayed*100
-                StatsScreen.PVMlostPercentage = StatsScreen.PVMlost/StatsScreen.PVMtotalPLayed*100
-            StatsScreen.PVMbestScore = pvm[3]
-            StatsScreen.PVMbestScoreTime = pvm[7]
-            StatsScreen.PVMbestTime = pvm[8]
+            PVPbestTime.text = str(divmod(pvp[8], 60)[0])+"m"+str(divmod(pvp[8], 60)[1])+"s"
+            
+            #pvm data
+            PVMtotalPLayed.text = str(pvm[1])
+            PVMwins.text = str(pvm[2])
+            PVMlost.text = str(pvm[5])
+            PVMdraw.text = str(pvm[6])
+            if pvm[1] != 0:
+                PVMwinsPercentagePB.value,PVMwinsPercentage.text = int(pvm[2]/pvm[1]*100), str(int(pvm[2]/pvm[1]*100))+"%"
+                PVMlostPB.value, PVMlostPercentage.text = int(pvm[5]/pvm[1]*100), str(int(pvm[5]/pvm[1]*100))+"%"
+                PVMdrawPB.value, PVMdrawPercentage.text = int(pvm[6]/pvm[1]*100), str(int(pvm[6]/pvm[1]*100))+"%"
+            PVMbestScore.text = str(pvm[3])+"pts | "+str(divmod(pvm[7], 60)[0])+"m"+str(divmod(pvm[7], 60)[1])+"s" 
+            
+            PVMbestTime.text = str(divmod(pvm[8], 60)[0])+"m"+str(divmod(pvm[8], 60)[1])+"s"
